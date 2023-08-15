fb202a69b329982d30058725e57f320443579439
VBulat74 <Vladimir.Bulat@gmail.com>
2023-08-15 19:08:29 +0300

    Lesson 1

---
+++ app/src/main/java/ru/com/bulat/notesmvvm/screens/StartFragment.kt
@@ -0,0 +1,19 @@
+package ru.com.bulat.notesmvvm.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.com.bulat.notesmvvm.R
+
+
+class StartFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
